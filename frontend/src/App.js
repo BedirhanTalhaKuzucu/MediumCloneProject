b0d461e7ad039bf64c09de4f1673822b4665b835
btkkzc <btkkzc@gmail.com>
2022-07-14 16:19:00 +0200

    app changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import UserDashboard from "./pages/UserDashboard"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      < Home />
+      <UserDashboard />
+      {/* < Home /> */}
     </div>
   );
 }
