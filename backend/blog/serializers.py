9d9e5ae879a0e85c9d380541c9cb2f7fdd49b79b
btkkzc <btkkzc@gmail.com>
2022-07-19 23:23:04 +0200

    home page finish

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,6 +1,8 @@
 
 from rest_framework import serializers
 from .models import Story
+from users.models import UserProfile
+import pandas as pd
 
 
 class StorySerializer(serializers.ModelSerializer):
@@ -19,15 +21,24 @@ class StorySerializer(serializers.ModelSerializer):
         )
     
     def get_tags(self,obj):
-        return obj.tags.all().values('id','tag_name')
+        return obj.tags.all().values('tag_name')
 
     def get_creatorInfo(self, obj):
+        user_img =  UserProfile.objects.filter(user= obj.user).first()
+        request = self.context.get('request')
+        user_img = user_img.profile_photo.url
+        user_img = request.build_absolute_uri(user_img)
+        print(user_img)
+        
         context = {
             "first_name" : obj.user.first_name,
             "last_name": obj.user.last_name,
             "email": obj.user.email,
+            "user_img": user_img,
         }
         return  context
+
+
     def comments(self):
         return self.comment_set.all()
     
