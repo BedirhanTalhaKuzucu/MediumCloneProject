0d80186a1b5b304b20413a695b90c1e493453af6
Sule <aybilenden@gmail.com>
2022-08-09 16:25:44 +0300

    loading gif added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,47 +1,47 @@
-import Header from "../components/Header"
-import Sidebar from "../components/Sidebar/Sidebar"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Cards from "../components/Cards/Cards"
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Cards from "../components/Cards/Cards";
 import Categorys from "../components/Categorys";
 import { useAppState } from "../contexts/AppContext";
 import SignIn from "./SignIn";
 import LogIn from "./LogIn";
 import { useEffect } from "react";
-
+import Images from "../assets/Images";
 
 function Home() {
-
-  const {data, trendList } = useAppState()
-  
-
-  
-
+  const { data, trendList } = useAppState();
 
   return (
     <>
-    <SignIn />
-    <LogIn />
-    <Header  />
-    <Sidebar trendList = {trendList} />
-    <Container className="mt-5">
+      <SignIn />
+      <LogIn />
+      <Header />
+      <Sidebar trendList={trendList} />
+      <Container className="mt-5">
         <Row>
-            <Col md={12} lg={{ span: 5,  order: 'last' }}>
-                <Categorys/>
-            </Col>
-            <Col md={12} lg={{ span: 7,  order: 'first' }}>
-                { data ? 
-                data.map((blogCard)=> (
-                <Cards blog={blogCard}/> 
-                )) 
-                :
-                <h2>Loading</h2> }
-
-            </Col>
-      </Row>
-    </Container>
- 
+          <Col md={12} lg={{ span: 5, order: "last" }}>
+            <Categorys />
+          </Col>
+          <Col md={12} lg={{ span: 7, order: "first" }}>
+            {data ? (
+              data.map((blogCard) => <Cards blog={blogCard} />)
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={Images.loading} alt="loading gif" />
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
