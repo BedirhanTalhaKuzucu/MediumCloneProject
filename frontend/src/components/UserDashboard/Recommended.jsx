cb4c741d7d833e0f9343d411b930e57dadaefd10
Sule <aybilenden@gmail.com>
2022-08-20 14:53:11 +0300

    lazy load-pagination start

--- frontend/src/components/UserDashboard/Recommended.jsx
+++ frontend/src/components/UserDashboard/Recommended.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ArticleCard from "./ArticleCard";
 import { useState } from "react";
-import { useAppState } from "../../contexts/AppContext";
 import Images from "../../assets/Images";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Recommended = () => {
-  const { data } = useAppState();
+  const [items, setItems] = useState([]);
+
+  const [offset, setoffset] = useState(0);
+
+  const [hasMore, sethasMore] = useState(true);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const res = await fetch(`http://127.0.0.1:8000/blog/stories/?_offset=0`);
+      const data = await res.json();
+      console.log(data);
+      setItems(data.results);
+    };
+
+    getArticles();
+  }, []);
+
+  const fetchArticleCards = async () => {
+    const res = await fetch(
+      `http://127.0.0.1:8000/blog/stories/?_offset=${offset}`
+    );
+    const data = await res.json();
+    console.log(data);
+    return data.results;
+  };
+
+  const fetchData = async () => {
+    const commentsFormServer = await fetchArticleCards();
+
+    setItems([...items, ...commentsFormServer]);
+    if (commentsFormServer.length === 0 || commentsFormServer.length < 5) {
+      sethasMore(false);
+    }
+    setoffset(offset + 1);
+  };
 
   return (
-    <div>
-      {data ? (
-        data.map((item, key) => <ArticleCard key={key} data={item} />)
-      ) : (
+    <InfiniteScroll
+      dataLength={items.length} //This is important field to render the next data
+      next={fetchData}
+      hasMore={hasMore}
+      loader={
         <div
           style={{
             display: "flex",
@@ -21,8 +56,13 @@ const Recommended = () => {
         >
           <img src={Images.loading} alt="loading gif" />
         </div>
-      )}
-    </div>
+      }
+      endMessage={<b>Yay! You have seen it all</b>}
+    >
+      {items.map((item, index) => {
+        return <ArticleCard key={index} data={item} />;
+      })}
+    </InfiniteScroll>
   );
 };
 
