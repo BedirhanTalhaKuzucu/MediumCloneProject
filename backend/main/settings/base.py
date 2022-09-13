a5eaecb6bb36cd85e09183c8c2993f9e47296be8
Sule <aybilenden@gmail.com>
2022-09-13 21:48:34 +0300

    google account start

--- backend/main/settings/base.py
+++ backend/main/settings/base.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'dj_rest_auth',
     "corsheaders",
     'django_extensions',
+    'django.contrib.sites',
+    # google ile account için
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
 
     # apps
@@ -159,3 +165,28 @@ CORS_ALLOW_METHODS = [
 #         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 #     ]
 # }
+
+# google account için:
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
