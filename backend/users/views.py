35e35937f14c8f97c3186efe31f2afa7a0877467
btkkzc <btkkzc@gmail.com>
2022-10-06 16:12:00 +0200

    sign in errror fixed

--- backend/users/views.py
+++ backend/users/views.py
@@ -24,7 +24,7 @@ class RegisterView(CreateAPIView):
         user = serializer.save()
         token = Token.objects.create(user=user)
         data = serializer.data
-        data['token'] = token.key
+        data['key'] = token.key
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
 
