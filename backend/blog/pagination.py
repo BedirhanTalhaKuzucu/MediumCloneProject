1aea16659242439e7c5e60f2bbacc0e828befa2b
btkkzc <btkkzc@gmail.com>
2022-10-31 17:51:40 +0100

    for deploy

--- backend/blog/pagination.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
-from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
-
-
-class SearchBarLimitPagination(MultipleModelLimitOffsetPagination):
-    default_limit = 3
