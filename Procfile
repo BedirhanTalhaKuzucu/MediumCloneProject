26bd96e42e30a7a975f918e096d7c8bd9d4c397f
btkkzc <btkkzc@gmail.com>
2022-11-01 15:56:21 +0100

    Revert "deploy setting"
    This reverts commit d63c291e81bb69d68155d17635ae4966a0eca506.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn 'MediumCloneProject.wsgi'
--- main/settings/base.py
+++ main/settings/base.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,10 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 # Default primary key field type
